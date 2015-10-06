ae12c77d3571b0051c73283e0b8af8e49e8cd6bd
Arnout Engelen <arnouten@bzzt.net>
2015-10-06 21:05:43 +0200

    Order by creation date and take first 20

--- app.js
+++ app.js
@@ -2,8 +2,12 @@
 
 // Currently works for organisations of up to 100 users
 // If you need more, add pagination :)
+const max_users = 100;
+const max_events = 20;
 
 var GitHubApi = require("github");
+var async = require("async");
+var _ = require("underscore");
 const GitHubOauthToken = process.env.GITHUB_OAUTH_TOKEN;
 
 var github = new GitHubApi({
@@ -22,23 +26,34 @@ github.authenticate({
     token: GitHubOauthToken
 });
 
+function byCreationTime(a, b) {
+    return a.created_at < b.created_at ? 1 : -1;
+}
+
 github.orgs.getMembers({ 
     org: "Xebia",
-    per_page: 100 
+    per_page: max_users
 }, function(error, members) {
     if (error) {
         console.log("Error fetching members: ", error);
+        return;
     }
-    members.forEach(function(member){
-      github.events.getFromUser({
-          user: member.login,
-          per_page: 100
-      }, function(error, events) {
-          events.forEach(function(event){
-              var repo = "n/a";
-              if (event.repo) repo = event.repo.name;
-              console.log(event.created_at, event.actor.login, event.type, repo);
-          });
-      });
-    });
+
+    async.map(members,
+        function(member, callback){
+            github.events.getFromUser({
+                user: member.login,
+                per_page: max_events
+            }, callback);
+        },
+        function(err, eventss) {
+            _.flatten(eventss)
+             .sort(byCreationTime)
+             .slice(0, max_events)
+             .forEach(function(event){
+                var repo = "n/a";
+                if (event.repo) repo = event.repo.name;
+                console.log(event.created_at, event.actor.login, event.type, repo);
+            });
+        });
 });
